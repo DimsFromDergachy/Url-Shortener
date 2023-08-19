c48816406d99aeabb7847b6043df44e5d72c3e78
Dmitry Tyurnikov <DimsFromDergachy@outlook.com>
2023-08-19 12:27:53 +0400

    Add save handler

---
+++ internal/http-server/handlers/url/save/save.go
@@ -0,0 +1,64 @@
+package save
+
+import (
+    "errors"
+    "io"
+    "log/slog"
+    "net/http"
+
+    resp "github.com/DimsFromDergachy/Url-Shortener/internal/lib/api/response"
+    "github.com/DimsFromDergachy/Url-Shortener/internal/lib/logger/sl"
+    "github.com/go-chi/chi/v5/middleware"
+    "github.com/go-chi/render"
+)
+
+type Request struct {
+    URL   string `json:"url" validate:"required,url"`
+    Alias string `json:"alias,omitempty"`
+}
+
+type Response struct {
+    resp.Response
+    Alias  string `json:"alias"`
+}
+
+type URLSaver interface {
+    SaveURL(URL, alias string) (int64, error)
+}
+
+func New(log *slog.Logger, urlSaver URLSaver) http.HandlerFunc {
+    return func(w http.ResponseWriter, r *http.Request) {
+        const op = "handlers.url.save.New"
+
+        log = log.With(
+            slog.String("op", op),
+            slog.String("request_id", middleware.GetReqID(r.Context())),
+        )
+
+        var req Request
+
+        err := render.DecodeJSON(r.Body, &req)
+        if errors.Is(err, io.EOF) {
+            log.Error("request body is empty")
+
+            render.JSON(w, r, resp.Response {
+                Status: resp.StatusError,
+                Error: "empty request",
+            })
+
+            return
+        }
+        if err != nil {
+            log.Error("failed to decode request body", sl.Err(err))
+
+            render.JSON(w, r, resp.Response {
+                Status: resp.StatusError,
+                Error: "failed to decode request",
+            })
+
+            return
+        }
+
+        log.Info("request body decoded", slog.Any("req", req))
+    }
+}
