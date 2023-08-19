95fe6ffb8b75ba14eaec27ed3721ebf86384c2f4
Dmitry Tyurnikov <DimsFromDergachy@outlook.com>
2023-08-19 13:13:17 +0400

    Generate alias & save URL to the storage

--- internal/http-server/handlers/url/save/save.go
+++ internal/http-server/handlers/url/save/save.go
@@ -8,6 +8,8 @@ import (
 
     resp "github.com/DimsFromDergachy/Url-Shortener/internal/lib/api/response"
     "github.com/DimsFromDergachy/Url-Shortener/internal/lib/logger/sl"
+    "github.com/DimsFromDergachy/Url-Shortener/internal/lib/random"
+    "github.com/DimsFromDergachy/Url-Shortener/internal/storage"
     "github.com/go-chi/chi/v5/middleware"
     "github.com/go-chi/render"
     "github.com/go-playground/validator"
@@ -22,6 +24,8 @@ type Response struct {
     resp.Response
     Alias  string `json:"alias"`
 }
+
+const aliasLength = 6
 
 type URLSaver interface {
     SaveURL(URL, alias string) (int64, error)
@@ -65,5 +69,37 @@ func New(log *slog.Logger, urlSaver URLSaver) http.HandlerFunc {
 
             return
         }
+
+        alias := req.Alias
+        if alias == "" {
+            alias = random.NewRandomString(aliasLength)
+        }
+
+        id, err := urlSaver.SaveURL(req.URL, alias)
+        if errors.Is(err, storage.ErrURLExists) {
+            log.Info("url already exists", slog.String("url", req.URL))
+
+            render.JSON(w, r, resp.Error("URL already exists"))
+
+            return
+        }
+        if err != nil {
+            log.Error("failed to add url", sl.Err(err))
+
+            render.JSON(w, r, resp.Error("failed to add URL"))
+
+            return
+        }
+
+        log.Info("url added", slog.Int64("id", id))
+
+        responseOK(w, r, alias)
     }
+}
+
+func responseOK(w http.ResponseWriter, r *http.Request, alias string) {
+    render.JSON(w, r, Response{
+        Response: resp.OK(),
+        Alias: alias,
+    })
 }
